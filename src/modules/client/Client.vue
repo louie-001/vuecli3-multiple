3b3f83c9ca8afa59788b0b2e30a69314719e3cf6
louie-001 <louie@xonro.com>
2018-10-09 10:25:35 +0800

    install

---
+++ src/modules/client/Client.vue
@@ -0,0 +1,18 @@
+/**
+*
+* @author louie
+* @date created in 2018-10-8 22:49
+*/
+<template>
+    <div id="client"></div>
+</template>
+
+<script>
+    export default {
+        name: "client"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/modules/console/Console.vue
@@ -0,0 +1,18 @@
+/**
+*
+* @author louie
+* @date created in 2018-10-8 22:48
+*/
+<template>
+    <div id="console"></div>
+</template>
+
+<script>
+    export default {
+        name: "console"
+    }
+</script>
+
+<style scoped>
+
+</style>
